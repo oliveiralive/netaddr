4b87ea88d303a8cee76df522c22c83c02f6f7a56
Emerson Silva <oliveiras.de.emerson@hotmail.com>
2021-07-25 11:20:37 -0300

    Translated to English

--- IP_on_the_Range.py
+++ IP_on_the_Range.py
@@ -1,19 +1,19 @@
 from netaddr import *
 
-#Redes Reservadas
+#Private Networks https://datatracker.ietf.org/doc/html/rfc1918
 RFC1918_10 = IPNetwork('10.0.0.0/8')
 RFC1918_172 = IPNetwork('172.16.0.0/12')
 RFC1918_192 = IPNetwork('192.168.0.0/16')
 
-#ip de teste
+#Test IP
 ip = '172.25.25.0'
 
 if IPAddress(ip) in ((IPNetwork(RFC1918_10))):
-    print('encontrei', ip)
+    print('Found', ip)
 elif IPAddress(ip) in ((IPNetwork(RFC1918_172))):
-    print('encontrei', ip)
+    print('Found', ip)
 elif IPAddress(ip) in ((IPNetwork(RFC1918_192))):
-    print('encontrei', ip)
+    print('Found', ip)
 else:
-    print(f'O IP {ip} esta fora do escopo')
+    print(f'The Ip {ip} is out of range')
 

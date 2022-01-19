001333858939dc1652700a93a075ba214b7b86ba
Emerson Silva <35010493+oliveiralive@users.noreply.github.com>
2022-01-19 12:04:05 -0300

    library replaced
    i identified that the netaddr library had a lot of problems and replaced it with this native python library

--- IP_on_the_Range.py
+++ IP_on_the_Range.py
@@ -1,19 +1,26 @@
-from netaddr import *
+# IMPORTS
+from ipaddress import IPv4Network
+from ipaddress import IPv4Address
 
-#Private Networks https://datatracker.ietf.org/doc/html/rfc1918
-RFC1918_10 = IPNetwork('10.0.0.0/8')
-RFC1918_172 = IPNetwork('172.16.0.0/12')
-RFC1918_192 = IPNetwork('192.168.0.0/16')
 
-#Test IP
-ip = '172.25.25.100'
+# VERIABLES - PRIVATES NETWORKS https://datatracker.ietf.org/doc/html/rfc1918
+RFC1918_10 = IPv4Network('10.0.0.0/8')
+RFC1918_172 = IPv4Network('172.16.0.0/12')
+RFC1918_192 = IPv4Network('192.168.0.0/16')
 
-if IPAddress(ip) in ((IPNetwork(RFC1918_10))):
-    print('Found', ip)
-elif IPAddress(ip) in ((IPNetwork(RFC1918_172))):
-    print('Found', ip)
-elif IPAddress(ip) in ((IPNetwork(RFC1918_192))):
-    print('Found', ip)
+# TEST IP
+ip = '10.140.140.2'
+
+#LOOP CHECK
+if IPv4Address(ip) in ((IPv4Network(RFC1918_10))):
+        print('Class C found', ip)
+elif IPv4Address(ip) in ((IPv4Network(RFC1918_172))):
+        print('Class B found', ip)
+elif IPv4Address(ip) in ((IPv4Network(RFC1918_192))):
+        print('Class A found', ip)
 else:
-    print(f'The Ip {ip} is out of range')
+    print('Its not a internal IP')
+
+
+
 
